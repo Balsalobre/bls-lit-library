6718c63a3ed5195bdca6477099a84a30ac692883
balsalobre <carlosocb@gmail.com>
2021-11-24 21:29:57 +0100

    Initial commit

---
+++ test/bls-lib.test.ts
@@ -0,0 +1,55 @@
+import { html } from 'lit';
+import { fixture, expect, assert } from '@open-wc/testing';
+import { BlsLib } from '../src/BlsLib.js';
+import '../src/bls-lib.js';
+
+let el: BlsLib;
+// Clean and initialize the component
+const cleanComponent = async () => {
+  await fixture<BlsLib>(html`<bls-lib></bls-lib>`);
+  await el.updateComplete;
+}
+
+describe('BlsLib', () => {
+
+  describe('Default input value', () => {
+    cleanComponent();
+    it('Props value', () => {
+      expect(el.description).to.equal('Default description');
+      assert.typeOf(el.description, 'String');
+
+      expect(el.label).to.equal('Default Text');
+      assert.typeOf(el.label, 'String');
+
+      expect(el.placeholder).to.equal('Default Text');
+      assert.typeOf(el.placeholder, 'String');
+    });
+  });
+    
+
+  // it('has a default title "Hey there" and counter 5', async () => {
+  //   const el = await fixture<BlsLib>(html`<bls-lib></bls-lib>`);
+
+  //   expect(el.title).to.equal('Hey there');
+  //   expect(el.counter).to.equal(5);
+  // });
+
+  // it('increases the counter on button click', async () => {
+  //   const el = await fixture<BlsLib>(html`<bls-lib></bls-lib>`);
+  //   el.shadowRoot!.querySelector('button')!.click();
+
+  //   expect(el.counter).to.equal(6);
+  // });
+
+  // it('can override the title via attribute', async () => {
+  //   const el = await fixture<BlsLib>(html`<bls-lib title="attribute title"></bls-lib>`);
+
+  //   expect(el.title).to.equal('attribute title');
+  // });
+
+  // it('passes the a11y audit', async () => {
+  //   const el = await fixture<BlsLib>(html`<bls-lib></bls-lib>`);
+
+  //   await expect(el).shadowDom.to.be.accessible();
+  // });
+});
